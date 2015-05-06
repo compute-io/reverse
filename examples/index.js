e2a856fdd88044286d16007e442bc0fae7476ea0
kgryte <kgryte@gmail.com>
2015-05-05 17:49:19 -0700

    [UPDATE] dotfiles. remove preamble. README. validators. copy option. [TESTS] arg validation. copy.

--- examples/index.js
+++ examples/index.js
@@ -1,7 +1,8 @@
+'use strict';
+
 var reverse = require( './../lib' );
 
 var arr = new Array( 100 );
-
 for ( var i = 0; i < arr.length; i++ ) {
 	arr[ i ] = i;
 }
--- test/test.js
+++ test/test.js
@@ -1,3 +1,5 @@
+/* global require, describe, it */
+'use strict';
 
 // MODULES //
 
@@ -17,7 +19,6 @@ var expect = chai.expect,
 // TESTS //
 
 describe( 'compute-reverse', function tests() {
-	'use strict';
 
 	it( 'should export a function', function test() {
 		expect( reverse ).to.be.a( 'function' );
@@ -25,20 +26,19 @@ describe( 'compute-reverse', function tests() {
 
 	it( 'should throw an error if not provided an array', function test() {
 		var values = [
-				5,
-				'5',
-				true,
-				NaN,
-				undefined,
-				null,
-				function(){},
-				{}
-			];
+			5,
+			'5',
+			true,
+			NaN,
+			undefined,
+			null,
+			function(){},
+			{}
+		];
 
 		for ( var i = 0; i < values.length; i++ ) {
 			expect( badValue( values[i] ) ).to.throw( TypeError );
 		}
-
 		function badValue( value ) {
 			return function() {
 				reverse( value );
@@ -46,37 +46,113 @@ describe( 'compute-reverse', function tests() {
 		}
 	});
 
+	it( 'should throw an error if provided an options argument which is not an object', function test() {
+		var values = [
+			5,
+			'5',
+			true,
+			NaN,
+			undefined,
+			null,
+			function(){},
+			[]
+		];
+
+		for ( var i = 0; i < values.length; i++ ) {
+			expect( badValue( values[i] ) ).to.throw( TypeError );
+		}
+		function badValue( value ) {
+			return function() {
+				reverse( [1,2,3], value );
+			};
+		}
+	});
+
+	it( 'should throw an error if provided a copy option which is not a boolean primitive', function test() {
+		var values = [
+			5,
+			'5',
+			new Boolean( true ),
+			NaN,
+			undefined,
+			null,
+			function(){},
+			[],
+			{}
+		];
+
+		for ( var i = 0; i < values.length; i++ ) {
+			expect( badValue( values[i] ) ).to.throw( TypeError );
+		}
+		function badValue( value ) {
+			return function() {
+				reverse( [1,2,3], {
+					'copy': value
+				});
+			};
+		}
+	});
+
 	it( 'should reverse an array', function test() {
-		var arr = [1,2,3,4],
-			expected = [4,3,2,1];
+		var arr, expected, actual;
 
-		reverse( arr );
+		arr = [1,2,3,4];
+		expected = [4,3,2,1];
 
-		assert.deepEqual( arr, expected );
+		actual = reverse( arr );
+
+		assert.deepEqual( actual, expected );
+	});
+
+	it( 'should mutate an array by default', function test() {
+		var arr, out;
+
+		arr = [1,2,3,4];
+		out = reverse( arr );
+
+		assert.strictEqual( arr, out );
 	});
 
 	it( 'should reverse an array of arbitrary element types', function test() {
-		var foo = function() {},
-			arr = [
-				undefined,
-				null,
-				false,
-				undefined,
-				foo,
-				NaN
-			],
-			expected = [
-				NaN,
-				foo,
-				undefined,
-				false,
-				null,
-				undefined
-			];
+		var arr, expected, actual;
 
-		reverse( arr );
+		arr = [
+			undefined,
+			null,
+			false,
+			undefined,
+			foo,
+			NaN
+		];
+		expected = [
+			NaN,
+			foo,
+			undefined,
+			false,
+			null,
+			undefined
+		];
 
-		assert.deepEqual( arr, expected );
+		actual = reverse( arr, {} );
+
+		assert.deepEqual( actual, expected );
+		assert.strictEqual( arr, actual );
+
+		function foo(){}
+	});
+
+	it( 'should reverse an array and return a copy', function test() {
+		var arr, expected, actual;
+
+		arr = [1,2,3,4];
+		expected = [4,3,2,1];
+
+		actual = reverse( arr, {
+			'copy': true
+		});
+
+		assert.deepEqual( actual, expected );
+		assert.ok( arr !== actual );
 	});
 
 });
